1c946579af62b1ba88493558a28882929840e5a9
sararat <sararat.wan@gmail.com>
2024-10-13 19:23:44 +0700

    13/10/257

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MyBar from './components/MyBar.vue';
+</script>
+
+<template>
+  <header>
+    <MyBar />
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+</style>
