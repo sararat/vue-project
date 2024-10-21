3e1be4a3e5eb710012b55b9c6ea9d2c6462d3226
sararat <sararat.wan@gmail.com>
2024-10-21 19:26:39 +0700

    21/10/67

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MyBar from './components/MyBar.vue';
 import MyCard from './components/MyCard.vue';
+import items from './Products.json'
 </script>
 
 <template>
@@ -9,13 +10,9 @@ import MyCard from './components/MyCard.vue';
   </header>
   <!--ใส่เนื้อหาเว็บตรงนี้-->
   <main>
-    <h1 style="padding-left: 10px">Attractions</h1>
+    <h1 style="padding-left: 10px">Products</h1>
     <div className="grid-container">
-      <MyCard />
-      <MyCard />
-      <MyCard />
-      <MyCard />
-      <MyCard />
+      <MyCard v-for="item in items" v-bind="item" />
     </div>
   </main>
 </template>
@@ -25,6 +22,8 @@ import MyCard from './components/MyCard.vue';
   display: grid;
   grid-column-gap: 0.5rem;
   grid-row-gap: 0.5rem;
+  justify-content: center;
+
 }
 @media (min-width: 576px) {
   .grid-container { grid-template-columns: repeat(1, 1fr); }
--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -1,31 +1,62 @@
-<template lang="pug">
+<template lang="">
   <div class="card">
-    <img src="https://www.mecallapi.com/attractions/1.jpg" alt="Phi Phi Islands" style="width:100%" />
+    <img :src="coverimage" :alt="name" style="width:100% " />
     <div className="container">
-      <h3>Phi Phi Islands</h3>
-      <p>Phi Phi Islands are a group of islands in Thailand between the large island of Phuket and the Malacca Coastal Strait of Thailand.</p>
+      <h1>{{name}}</h1>
+      <p class="price">{{price}}</p>
+      <p>{{detail}}</p>
+      <p><button>Add to Cart</button></p>
     </div>
   </div>
 </template>
 <script>
 export default {
-  
+  props: ['id', 'name', 'detail','coverimage', 'price' ]
 }
 </script>
 <style scoped>
 .card {
   /* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  padding-bottom: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 350px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
 }
 /* On mouse-over, add a deeper shadow */
 .card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 10px 20px 0 rgba(0,0,0,0.2);
+}
+img {
+  border-radius: 5px 5px 0 0;
+  height: 350px;
 }
 /* Add some padding inside the card container */
 .container {
   padding: 2px 16px;
+  margin: 20px;
+  margin-top: 10px;
+}
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+  margin-top: 10px;
+}
+
+.card button:hover {
+  opacity: 0.7;
 }
 </style>
 
