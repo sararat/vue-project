0a47446c1b043fe76b06fa099a4ad8dc66d51476
sararat <sararat.wan@gmail.com>
2025-06-28 21:05:57 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base:'vue-project.git',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
